a0dcc31e7d8263289461bf104515ee153439e80c
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-01-18 11:54:28 -0800

    Update downloading scripts

--- roles/jenkins/files/jenkins_plugins_and_dependencies.py
+++ roles/jenkins/files/jenkins_plugins_and_dependencies.py
@@ -1,25 +1,49 @@
+import pathlib
+import shutil
+import sys
+import urllib.request
 import zipfile
 
-# dependecies_list = []
+dependecies_list, download_url = [], "https://updates.jenkins-ci.org/latest/"
 
 
-# def get_dependencies():
-# 	section, plain_str = False, ""
-# 	f = open('/Users/tsoliang/Downloads/META-INF/MANIFEST.MF', 'r')
-# 	for line in f:
-# 		if line.startswith('Plugin-Dependencies'):
-# 			section = True
-# 		elif line.startswith('Plugin-Developers'):
-# 			section = False
-# 		if section:
-# 			plain_str += line.rstrip().lstrip('Plugin-Dependencies:').lstrip(' ')
+def get_dependencies(plugin):
+	# unzip plugin file
+	zip_ref = zipfile.ZipFile(plugin, 'r')
+	zip_ref.extract('META-INF/MANIFEST.MF', '.')
+	zip_ref.close()
 
-# 	for item in plain_str.split(','):
-# 		dependecies_list.append(item.split(':')[0])
+	# analyze plugin dependencies list
+	section, plain_str = False, ""
+	f = open('META-INF/MANIFEST.MF', 'r')
+	for line in f:
+		if line.startswith('Plugin-Dependencies'):
+			section = True
+		elif line.startswith('Plugin-Developers'):
+			section = False
+		if section:
+			plain_str += line.rstrip().lstrip('Plugin-Dependencies:').lstrip(' ')
 
-# get_dependencies()
-# print(dependecies_list)
+	for item in plain_str.split(','):
+		item = item.split(':')[0]
+		if item not in dependecies_list:
+			dependecies_list.append(item)
 
-zip_ref = zipfile.ZipFile('/Users/tsoliang/Downloads/git.hpi', 'r')
-zip_ref.extract('META-INF/MANIFEST.MF', '.')
-zip_ref.close()
+	# clean meta directory
+	shutil.rmtree('META-INF')
+
+	i = 0
+	while i < len(dependecies_list):
+		if not pathlib.Path(item).is_file():
+			download_plugin(item)
+		i += 1
+
+
+def download_plugin(plugin):
+	print('Now downloading ' + plugin + '...')
+	plugin += ".hpi"
+	urllib.request.urlretrieve(download_url + plugin, plugin)
+	get_dependencies(plugin)
+
+if __name__ == '__main__':
+	download_plugin(sys.argv[1])
